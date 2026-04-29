10b1c1643b6cb0abcb4a0b8dcd932db49ef0d24c
Dolinka Márk Gergely <dolinkamark@hotmail.com>
2026-04-29 13:12:34 +0200

    Wire in WasteManagementContext to Azure functions

--- src/Ymir.GeminiSync.SyncFunction/Program.cs
+++ src/Ymir.GeminiSync.SyncFunction/Program.cs
@@ -1,7 +1,12 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ymir.GeminiSync.Domain.Repositories;
+using Ymir.GeminiSync.EntityFramework;
+using Ymir.GeminiSync.EntityFramework.Repositories;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -10,5 +15,9 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+
+builder.Services.AddDbContext<WasteManagementContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WasteManagementContext")));
+builder.Services.AddTransient<IGarbageBinCollectionRepository, GarbageBinCollectionRepository>();
 
 builder.Build().Run();
