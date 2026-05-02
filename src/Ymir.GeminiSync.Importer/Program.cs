a05a7291ca9628743dee79e02931142cb2a52fcb
Dolinka Márk Gergely <dolinkamark@hotmail.com>
2026-05-02 17:37:06 +0200

    Move ImporterWorker to Importer

--- src/Ymir.GeminiSync.Importer/Program.cs
+++ src/Ymir.GeminiSync.Importer/Program.cs
@@ -1,10 +1,19 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using Ymir.GeminiSync.Domain.Repositories;
 using Ymir.GeminiSync.EntityFramework;
 using Ymir.GeminiSync.EntityFramework.Repositories;
 using Ymir.GeminiSync.Importer;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSerilog((services, loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext();
+});
 
 builder.Services.AddDbContext<WasteManagementContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("WasteManagement")));
