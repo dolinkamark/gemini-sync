1db51ec0c05488cae0f313dc7c4b9b1f0071640e
Dolinka Márk Gergely <dolinkamark@hotmail.com>
2026-05-12 10:10:47 +0200

    refactor: Imrpove the garbage collection line mapping

--- src/Ymir.GeminiSync.Domain/GarbageBinCollectionLine.cs
+++ src/Ymir.GeminiSync.Domain/GarbageBinCollectionLine.cs
@@ -3,16 +3,19 @@
 public class GarbageBinCollectionLine
 {
     public int CustomerId { get; set; }
-    public int AgreementLineId { get; set; }
-    public int? AgreementId { get; set; }
+    public long AgreementLineId { get; set; }
+    public long? AgreementId { get; set; }
 
-    public int? ExternalAgreementId { get; set; }
-    public int? Termin { get; set; }
-    public int UnitId { get; set; }
-
+    public string ExternalAgreementId { get; set; }
+    public string UnitId { get; set; }
     public int PlaceNr { get; set; }
+
+    public int? Termin { get; set; }
+
     public string FractionName { get; set; }
-    public string ShortName { get; set; }
+
+    public int ShortName { get; set; }
+
     public bool HasLock { get; set; }
 
     public DateTime RegDate { get; set; }
